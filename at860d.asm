8344bc1cec46e5cd0f651b9fb5dc5e3828cfcf15
Tommie Gannert <tommie@gannert.se>
2021-11-19 18:32:00 +0100

    Implements standby reed relay management.
    A 20 s standby delay. Max for a single byte is 25 s with the current
    timer settings.
    
    Increases timer_value to 16 bits.

--- at860d.asm
+++ at860d.asm
@@ -8,6 +8,9 @@
 #define TRIACZCC_NUM_FRAC_BITS 5
 #define TRIACZCC_NUM_EXTRA_BITS 3
 
+;; In 20 s in 128 ms ticks.
+#define STANDBY_DELAY (20000 / 128)
+
     udata
 
 #define section_udata
@@ -55,10 +58,8 @@ loop:
     include "modules.inc"
 #undefine section_idle
 
-    movf        adc_temp_value, W
-    movwf       w16
-    clrf        w16 + 1
-    display_set_temp_w16
+    standby_skip_if_not_active
+    goto        in_standby
 
     movlw       120
     airpump_setw
@@ -66,10 +67,40 @@ loop:
     movf        adc_knob_value, W
     heater_setw
 
-    movf        airpump_value, W
+    movf        adc_temp_value, W
+    movwf       w16
+    clrf        w16 + 1
+    display_set_temp_w16
+
+    ;movf        airpump_value, W
+    movf        triaczcc_delay, W
     movwf       w16
     clrf        w16 + 1
     display_set_air_w16
+
+    movlw       HIGH loop
+    movwf       PCLATH
+    goto        loop
+
+in_standby:
+    movlw       32
+    airpump_setw
+
+    clrw
+    heater_setw
+
+    movlw       LSA & LSC & LSD & LSF & LSG ; "S"
+    movwf       display_buf + 2
+    movlw       LSD & LSE & LSF             ; "L"
+    movwf       display_buf + 1
+    movlw       LSA & LSB & LSE & LSF & LSG ; "P"
+    movwf       display_buf + 0
+
+    movlw       ~0
+    movwf       display_buf + 3
+    movwf       display_buf + 4
+    movwf       display_buf + 5
+    movwf       display_buf + 6
 
     movlw       HIGH loop
     movwf       PCLATH
